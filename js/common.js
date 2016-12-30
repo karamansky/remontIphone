1c9ad81f807c196f3db4ddbdd8bec3124733a688
karamansky <karamansky@gmail.com>
2016-12-30 22:13:43 +0200

    Коммит перед установкой SSD

--- js/common.js
+++ js/common.js
@@ -32,6 +32,238 @@ $(function(){
 		});
 
 
+	//cart
+		if(getCookie('cartFormData') != undefined){
+			restoreCartForm();
+			TotalSum();
+		}
+		
+		TotalSum();
+		countItems();
+
+		//добавление кол-ва товаров в корзине
+		$(document).on("click",".cart__action-btn.plus",function(){
+			event.preventDefault();
+			var tmp_count = $(this).parent().prevAll(".cart__kol-vo").children("span").html();
+			tmp_count++;
+			// console.log(tmp_count);
+			$(this).parent().prevAll(".cart__kol-vo").children("span").html(tmp_count);
+			TotalItemSum($(this));
+			TotalSum();
+			countItems();
+		});
+		//уменьшение кол-ва товаров в корзине
+		$(document).on("click",".cart__action-btn.minus",function(){
+			event.preventDefault();
+			var tmp_count = $(this).parent().prevAll(".cart__kol-vo").children("span").html();
+			if(tmp_count > 1){
+				tmp_count--;
+				// console.log(tmp_count);
+				$(this).parent().prevAll(".cart__kol-vo").children("span").html(tmp_count);
+			}
+			TotalItemSum($(this));
+			TotalSum();
+			countItems();
+		});
+		//удаление товара из корзины
+		$(document).on("click",".cart__action-btn.delete",function(){
+			event.preventDefault();
+			$(this).parent().parent().slideUp().html("");
+			//проверка корзины на пустоту
+			if($("table tr").is("td")){
+				$(".cart table").slideUp();
+				$(".cart__empty").show();
+			}
+			TotalSum();
+			isEmpty();
+		});
+
+
+		// if(Number($(".total").html()) == 0){
+		// 	$("#OrderCart").hide();
+		// }else{
+		// 	TotalSum();
+		// }
+
+		/**
+		 * Добавление в корзину
+		 */
+		// $(".moveToCart").on("click",function(){
+		// 	var price = Number($($(this).prevAll().find(".goods-price")).html());
+		// 	var name = $($(this).parent().find(".goods-name")).html();
+		// 	var img = $($(this).parent().find("img")).attr("src");
+		// 	addToCart(price, name, img);
+		// 	TotalSum();
+		// });
+
+		/**
+		 * Корзина пуста ?
+		 */
+		function isEmpty(){
+			if (countItems() <= 0){
+				$(".cart__table table, .cart__total, .cart__form").hide();
+				$(".cart__empty").show(400);
+			}
+		}
+
+		/**
+		 * Функция подсчета суммы корзины
+		 */
+		function TotalItemSum(obj){
+			var total = 0;
+			var items = obj.parent().prevAll(".cart__kol-vo").children("span").html();
+			var price = obj.parent().prevAll(".cart__price").children("span").html();
+					total = Number(items*price);
+
+			obj.parent().prevAll(".cart__sum").children("span").html(total);
+			// console.log(total);
+			return total;
+		}
+
+		/**
+		 * Функция подсчета суммы корзины
+		 */
+		function TotalSum(){
+			var total = 0;
+
+			$(".cart__sum span").each(function(){
+				total += Number($(this).html());
+			});
+			$("span.total").html(total);
+
+			// console.log(total);
+
+			$("input[name=money]").val(total);
+			return total;
+		}
+		/**
+		 * Функция подсчета кол-ва единиц в корзине
+		 */
+		function countItems(){
+			var totalItems = 0;
+
+			$(".cart__kol-vo span").each(function(){
+				totalItems += Number($(this).html());
+			});
+			$("span.total-count").html(totalItems);
+			$("input[name=items]").val(totalItems);
+			return totalItems;
+		}
+
+		/**
+		 * Функция добавления в корзину
+		 */
+		function addToCart(price, name, img){
+			var htmlItem =
+					"<div class='row cart-item'><div class='col-sm-3 cart-item-img'><img src='"+ img +"' alt=''/></div>"+
+					"<div class='col-sm-5 cart-item-name'><span>"+ name +"</span></div>"+
+					"<div class='col-sm-3 cart-item-price'><span>"+ price +"</span> р.</div>"+
+					"<input type='hidden' name='goods[]' value='" + name + " = " + price + "'>"+
+					"<div class='col-sm-1 cart-item-delete' title='Удалить'><i class='fa fa-close'></i></div>";
+			$(".cart-table").append(htmlItem);
+			setFormCookie();
+		}
+
+		/**
+		 * Функция Удаление товара из корзины
+		 */
+		function delCartItem(cartClass){
+			$(document).on("click",cartClass, function(){
+				$(this).parent().remove();
+				TotalSum();
+				setFormCookie();
+			});
+		}
+
+		/**
+		 * Функция записывает форму в куки
+		 */
+		function setFormCookie(){
+			var cartArray = new Array();
+			$(".cart-table .cart-item").each(function(){
+				var img     = $(this).find(".cart-item-img img").attr("src");
+				var name    = $(this).find(".cart-item-name span").html();
+				var price   = Number($(this).find(".cart-item-price span").html());
+				var tmp = {"img":img,"name":name,"price":price};
+				cartArray.push(tmp);
+			});
+			var str = JSON.stringify(cartArray);
+			// запоминаем параметры формы в куки
+			setCookie('cartFormData', str);
+			//    console.log(getCookie("cartFormData"));
+		}
+
+		/**
+		 * Функция Восстановление данных формы из куки
+		 */
+		function restoreCartForm(){
+			var $form = $('#cartForm');
+			var formData = getCookie('cartFormData');
+			if(formData == undefined) {
+				return;
+			}
+			else{
+				var item = new Array();
+				item = JSON.parse(formData);
+				for(var i=0; i<item.length; i++){
+					addToCart(item[i].price,item[i].name, item[i].img);
+				}
+			}
+		}
+
+		/**
+		 * Функция очистки корзины
+		 */
+		function ClearCart(){
+			deleteCookie('cartFormData');
+			$(".cart-table").html("");
+			$("#delivery-cash").html("0");
+			TotalSum();
+		}
+
+		/**
+		 *  возвращает cookie если есть или undefined
+		 */
+		function getCookie(name) {
+			var matches = document.cookie.match(new RegExp(
+					"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+			))
+			return matches ? decodeURIComponent(matches[1]) : undefined
+		}
+
+		/**
+		 *  устанавливает cookie
+		 */
+		function setCookie(name, value, props) {
+			props = props || {};
+			var exp = props.expires
+			if (typeof exp == "number" && exp) {
+				var d = new Date();
+				d.setTime(d.getTime() + exp*1000);
+				exp = props.expires = d;
+			}
+			if(exp && exp.toUTCString) { props.expires = exp.toUTCString(); }
+
+			value = encodeURIComponent(value);
+			var updatedCookie = name + "=" + value;
+			for(var propName in props){
+				updatedCookie += "; " + propName;
+				var propValue = props[propName];
+				if(propValue !== true){ updatedCookie += "=" + propValue; }
+			}
+			document.cookie = updatedCookie;
+		}
+
+		/**
+		 *  удаляет cookie
+		 */
+		function deleteCookie(name) {
+			setCookie(name, null, { expires: -1 });
+		}
+
+
+
+
 	//tabs
 		$('.tab-content1 > div').hide();
 		$('.tab-content1 > div').first().slideDown();
